85f867ce11dbacaad6a9bb448f4aeae5f39d3f68
Nico <nicolasdortu@outlook.com>
2025-08-03 21:56:33 +0200

    Add boilerplate helmet

---
+++ Content/Armor/YonduHelmet.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace YonduArrow.Content.Armor
+{
+    public class YonduHelmet : ModItem
+    {
+        // public static readonly int AdditiveGenericDamageBonus = 20;
+
+        // public static LocalizedText SetBonusText { get; private set; }
+
+        public override void SetStaticDefaults()
+        {
+            // If your head equipment should draw hair while drawn, use one of the following:
+            // ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false; // Don't draw the head at all. Used by Space Creature Mask
+            // ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true; // Draw hair as if a hat was covering the top. Used by Wizards Hat
+            // ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true; // Draw all hair as normal. Used by Mime Mask, Sunglasses
+            // ArmorIDs.Head.Sets.DrawsBackHairWithoutHeadgear[Item.headSlot] = true;
+
+            // SetBonusText = this.GetLocalization("SetBonus").WithFormatArgs(AdditiveGenericDamageBonus);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 22; // Width of the item
+            Item.height = 16; // Height of the item
+            Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+            Item.rare = ItemRarityID.Green; // The rarity of the item
+            Item.defense = 5; // The amount of defense the item will give when equipped
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.WoodenArrow, 100)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
